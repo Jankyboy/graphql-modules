31be5097e8689c42c7f9b1fe2b2ab4924029e9a2
Dotan Simha <dotansimha@gmail.com>
2026-05-26 12:08:42 +0300

    Revert "chore(deps): update dependency bob-the-bundler to v7 (#2639)"
    This reverts commit 0cc7337ba5ca380feb33aebac366206d389d181c.

--- scripts/fix-async-context.js
+++ scripts/fix-async-context.js
@@ -5,6 +5,13 @@ const __project = path.resolve(__dirname, '..');
 
 console.log('Fixing async context imports in dist package.json...');
 
+const distPkg = JSON.parse(
+  fs.readFileSync(
+    path.join(__project, 'packages', 'graphql-modules', 'dist', 'package.json'),
+    'utf8'
+  )
+);
+
 const srcPkg = JSON.parse(
   fs.readFileSync(
     path.join(__project, 'packages', 'graphql-modules', 'package.json'),
@@ -26,46 +33,24 @@ if (!node || !browser) {
   );
 }
 
-const modTypes = ['cjs', 'esm', '.'];
-
 for (const file of [node, browser]) {
   const src = path.join(__project, 'packages', 'graphql-modules', 'src', file);
-  for (const modType of modTypes) {
-    const dest = path.join(
-      __project,
-      'packages',
-      'graphql-modules',
-      'dist',
-      modType,
-      file
-    );
-    console.log(`Copying ${src} to ${dest}...`);
-    fs.copyFileSync(src, dest);
-  }
+  const dest = path.join(
+    __project,
+    'packages',
+    'graphql-modules',
+    'dist',
+    file
+  );
+  console.log(`Copying ${src} to ${dest}...`);
+  fs.copyFileSync(src, dest);
 }
 
 console.log('Updating imports field in dist package.json...');
-
-for (const modType of modTypes) {
-  try {
-    const distPath = path.join(
-      __project,
-      'packages',
-      'graphql-modules',
-      'dist',
-      modType,
-      'package.json'
-    );
-    const distFile = fs.readFileSync(distPath, 'utf8');
-    const distPkg = JSON.parse(distFile);
-    distPkg.imports = srcPkg.imports;
-
-    fs.writeFileSync(distPath, JSON.stringify(distPkg, null, 2) + '\n');
-  } catch (err) {
-    console.warn(
-      `Warning: Could not update imports field in dist package.json for module type "${modType}": ${err.message}`
-    );
-  }
-}
+distPkg.imports = srcPkg.imports;
+fs.writeFileSync(
+  path.join(__project, 'packages', 'graphql-modules', 'dist', 'package.json'),
+  JSON.stringify(distPkg, null, 2) + '\n'
+);
 
 console.log('OK');
